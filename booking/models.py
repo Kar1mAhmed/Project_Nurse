bbb4df3f37a4c325851aa1a8012de90eda1d1760
ahmedhamdy <ahmedhamdy7796@gmail.com>
2024-04-19 06:22:08 +0200

    custom models

--- booking/models.py
+++ booking/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.conf import settings
+from users.models import Nurse, Patient
 
 # Create your models here.
 
 
 
 class Booking(models.Model):
-    nurse = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='nurse_bookings')
-    patient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='patient_bookings')
-    appointment = models.DateTimeField(default= datetime(2024, 4, 18, 9, 0))
+    nurse = models.ForeignKey(Nurse, on_delete=models.CASCADE, related_name='nurse_bookings')
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='patient_bookings')
+    appointment = models.DateTimeField(default=datetime(24, 10, 3, 10, 55))
     price = models.DecimalField(max_digits=10, decimal_places=2)
